6703f73a3e44b52df3520a22d246a6f6e9ad24c5
Piotr Wiercinski <piotrwier@o2.pl>
2018-03-03 15:51:34 +0100

    style: add classnames

--- src/component/ButtonPanel.js
+++ src/component/ButtonPanel.js
@@ -10,7 +10,7 @@ class ButtonPanel extends React.Component {
 
 	render() {
 		return(
-			<div>
+			<div className="component-button-panel">
 
 				<div>
 					<Button name="AC" clickHandler={this.handleClick} />
